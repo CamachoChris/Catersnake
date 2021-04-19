08e0194cd0fa970dcfcd42b36fb798f0de8898de
Grimakar <christian.camacho@mail.de>
2021-04-19 22:32:40 +0200

    Add project files.

---
+++ Snake/Catersnake.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace SnakeModel
+{
+    public class Limb
+    {
+        public int X;
+        public int Y;
+    }
+    public enum Direction
+    {
+        Left,
+        Up,
+        Right,
+        Down
+    }
+
+    public class Catersnake
+    {
+        public List<Limb> limbs;
+        private readonly int _maxFieldX, _maxFieldY;
+
+        public Catersnake(int caterStartX, int caterStartY, int maxFieldX, int maxFieldY)
+        {
+            limbs = new List<Limb> { new Limb() { X = caterStartX, Y = caterStartY } };
+            _maxFieldX = maxFieldX;
+            _maxFieldY = maxFieldY;
+        }
+
+        public void Move(Direction direction)
+        {
+            SetNextHead(direction);
+            limbs.RemoveAt(limbs.Count - 1);
+        }
+
+        public void Grow(Direction direction)
+        {
+            SetNextHead(direction);
+        }
+
+        private void SetNextHead(Direction direction)
+        {
+            Limb newHead = new Limb() { X = limbs[0].X, Y = limbs[0].Y};
+            switch (direction)
+            {
+                case Direction.Left:
+                    if (newHead.X == 0)
+                        newHead.X = _maxFieldX - 1;
+                    else
+                        newHead.X--;
+                    break;
+                case Direction.Up:
+                    if (newHead.Y == 0)
+                        newHead.Y = _maxFieldY - 1;
+                    else
+                        newHead.Y--;
+                    break;
+                case Direction.Right:
+                    if (newHead.X == _maxFieldX - 1)
+                        newHead.X = 0;
+                    else
+                        newHead.X++;
+                    break;
+                case Direction.Down:
+                    if (newHead.Y == _maxFieldY - 1)
+                        newHead.Y = 0;
+                    else
+                        newHead.Y++;
+                    break;
+            }
+            limbs.Insert(0, newHead);
+        }
+
+        public void ShowCatersnake(string txt)
+        {
+            Debug.WriteLine(txt);
+            for (int i = 0; i < limbs.Count; i++)
+            {
+                Debug.WriteLine($"{i}. X={limbs[i].X}, Y={limbs[i].Y}");
+            }
+        }
+    }
+}
---
+++ Snake/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SnakeModel;
+
+namespace CatersnakeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string appName= "Catersnake";
+        const string version = "0.0.0";
+        const string developer = "Grimakar";
+        const string timeOfDevelopment = "April 2021";
+
+        const int CaterThickness = 24;
+        const int GridMultiplier = 20;
+
+        const int PlayingFieldWidth = 25;
+        const int PlayingFieldHeight = 25;
+
+        Catersnake snake = new Catersnake(13, 13, PlayingFieldWidth, PlayingFieldHeight);
+        Ellipse circle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            circle = new Ellipse() { Height = CaterThickness, Width = CaterThickness, Fill = Brushes.DarkGreen };
+            PlayingField.Children.Add(circle);
+
+        }
+
+        private void PaintCater()
+        {
+            PaintLimb(snake.limbs[0].X, snake.limbs[0].Y);
+        }
+
+        private void PaintLimb(int column, int row)
+        {
+            circle.Margin = new Thickness() { Left = GridMultiplier * column, Top= GridMultiplier * row };
+        }
+
+        private void MenuQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, $"{appName}\n{version}\n{timeOfDevelopment} {developer}.\nNo rights reserved...", $"About {appName}");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PaintCater();
+            /*snake.Grow(Direction.Left);
+            snake.Grow(Direction.Down);
+            snake.Grow(Direction.Right);
+            snake.Grow(Direction.Right);*/
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    snake.Move(Direction.Left);
+                    break;
+                case Key.Up:
+                    snake.Move(Direction.Up);
+                    break;
+                case Key.Right:
+                    snake.Move(Direction.Right);
+                    break;
+                case Key.Down:
+                    snake.Move(Direction.Down);
+                    break;
+            }
+            PaintCater();
+        }
+    }
+}
